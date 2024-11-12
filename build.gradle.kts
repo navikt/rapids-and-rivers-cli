e05248c96b8e93c6edc634b6dd2d86b9a6d14901
david steinsland <david@davidsteinsland.net>
2024-11-12 08:30:13 +0100

    fjerner gradleversjon fra build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ group = "com.github.navikt"
 version = properties["version"] ?: "local-build"
 
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.0.21"
     id("java")
     id("maven-publish")
 }
